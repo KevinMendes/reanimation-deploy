66b3f7aee18077f4459b146ee3877e648f397713
KevinMendes <mendes.kevin35@gmail.com>
2020-03-28 13:07:30 +0100

    test deploy #1

---
+++ public/precache-manifest.68ed5df97b4d256b9a709d3a5a689a9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "affa912a69ff7a7ffa76b1a8b68ad85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b82cc622df6612e571d",
+    "url": "/static/js/2.83de0493.chunk.js"
+  },
+  {
+    "revision": "6c5833db93cd7c0f813219a8bc75480b",
+    "url": "/static/js/2.83de0493.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b04abf9109f2b1dbe0",
+    "url": "/static/js/main.2d1a5c7a.chunk.js"
+  },
+  {
+    "revision": "a9458f4dede3547ba79d",
+    "url": "/static/js/runtime-main.23f759c9.js"
+  }
+]);
